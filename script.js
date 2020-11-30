f74bf87d53f447bdff9d43283d46fbcb43e6a6d9
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-30 16:33:11 -0500

    Prompt user for image size and adjust image

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let teamNames = ["Chris", "Michael"];
 
 function addNamesToPage(teamNames) {
-    let ulNames = document.querySelector('ul');
-    for (let i of teamNames) {
+  let ulNames = document.querySelector("ul");
+  for (let i of teamNames) {
     ulNames.innerHTML += `<li>${i}</li>`;
-}
+  }
 }
 addNamesToPage(teamNames);
+
+
+
+
+
+
+function setImageSize(userAnswer) {
+    const imgElement = document.querySelector("img");
+    userAnswer = userAnswer.toLowerCase();
+    if (userAnswer === "small") {
+        imgElement.style.width = "400px";
+        imgElement.style.height = "250px";
+    } else if (userAnswer === "large") {
+        imgElement.style.width = "800px";
+        imgElement.style.height = "500px";
+    } else {
+      alert("I don't understand");
+    }
+}
+
+const userAnswer = prompt("Do you want the image to be small or large?");
+setImageSize(userAnswer);
