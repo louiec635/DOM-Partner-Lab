a4be987c31beb4e289708490f42feb6d3904b58c
chris_l <louiec635@gmail.com>
2020-12-01 16:09:04 -0500

    Add solution to part two problem 1

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
-let teamNames = ["Chris", "Michael"];
+// let teamNames = ["Chris", "Michael"];
 
-function addNamesToPage(teamNames) {
-  let ulNames = document.querySelector("ul");
-  for (let i of teamNames) {
-    ulNames.innerHTML += `<li>${i}</li>`;
-  }
-}
-addNamesToPage(teamNames);
-
-
-
-
-
-setImageSize("small");
-function setImageSize(userAnswer) {
-    const imgElement = document.querySelector("img");
-    userAnswer = userAnswer.toLowerCase();
-    if (userAnswer === "small") {
-        imgElement.style.width = "400px";
-        imgElement.style.height = "250px";
-    } else if (userAnswer === "large") {
-        imgElement.style.width = "800px";
-        imgElement.style.height = "500px";
-    } else {
-      alert("I don't understand");
-    }
-}
-
-// const userAnswer = prompt("Do you want the image to be small or large?");
-// setImageSize(userAnswer);
+// function addNamesToPage(teamNames) {
+//   let ulNames = document.querySelector("ul");
+//   for (let i of teamNames) {
+//     ulNames.innerHTML += `<li>${i}</li>`;
+//   }
+// }
+// addNamesToPage(teamNames);
 
 
 
 
-// const userColor = prompt("Please enter a color");
 
-// let body = document.body;
-
-// function bodyColor (userColor) {
-//     body.style.backgroundColor = userColor;
+// setImageSize("small");
+// function setImageSize(userAnswer) {
+//     const imgElement = document.querySelector("img");
+//     userAnswer = userAnswer.toLowerCase();
+//     if (userAnswer === "small") {
+//         imgElement.style.width = "400px";
+//         imgElement.style.height = "250px";
+//     } else if (userAnswer === "large") {
+//         imgElement.style.width = "800px";
+//         imgElement.style.height = "500px";
+//     } else {
+//       alert("I don't understand");
+//     }
 // }
 
-// bodyColor(userColor);
+// // const userAnswer = prompt("Do you want the image to be small or large?");
+// // setImageSize(userAnswer);
 
 
 
 
-let teamMembers = [];
-let member = prompt("What is one of your team members?");
-teamMembers.push(member);
-addNamesToPage(teamMembers);
+// // const userColor = prompt("Please enter a color");
+
+// // let body = document.body;
+
+// // function bodyColor (userColor) {
+// //     body.style.backgroundColor = userColor;
+// // }
+
+// // bodyColor(userColor);
+
+
+
+
+// let teamMembers = [];
+// // let member = prompt("What is one of your team members?");
+// teamMembers.push(member);
+// addNamesToPage(teamMembers);
+
+
+let userName = prompt("Please enter your name");
+
+function userResume () {
+  let body = document.body;
+  body.innerHTML += `<h1>${userName}'s Resume!</h1>`;
+}
+
+userResume();
