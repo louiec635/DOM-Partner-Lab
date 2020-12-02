8f03bc9d32d2f112b89c0dfe8968484416ff7cf6
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-02 16:01:03 -0500

    Do part 3 step 1

--- script.js
+++ script.js
@@ -52,39 +52,56 @@
 // addNamesToPage(teamMembers);
 
 
-let userName = prompt("Please enter your name");
-
-function userResume () {
-  let body = document.body;
-  body.innerHTML += `<h1>${userName}'s Resume!</h1>`;
-}
-
-userResume();
 
 
 
-const skills = ["Javascript", "HTML", "CSS", "Microsoft Paint"];
-const listType = getListType();
-listSkills(listType, skills);
+// let userName = prompt("Please enter your name");
 
-function getListType() {
-  const userTag = prompt("Ordered or unordered list?").toLowerCase();
-  if (userTag === "ordered" || userTag === "ol") {
-    return "ol";
-  }
-  else if (userTag === "unordered" || userTag === "ul") {
-    return "ul";
-  }
-  else {
-    alert("I don't understand");
-  }
-}
+// function userResume () {
+//   let body = document.body;
+//   body.innerHTML += `<h1>${userName}'s Resume!</h1>`;
+// }
 
-function listSkills(listType, skills) {
-  const listElement = document.createElement(listType);
-  document.body.appendChild(listElement);
+// userResume();
+
+
+
+// const skills = ["Javascript", "HTML", "CSS", "Microsoft Paint"];
+// const listType = getListType();
+// listSkills(listType, skills);
+
+// function getListType() {
+//   const userTag = prompt("Ordered or unordered list?").toLowerCase();
+//   if (userTag === "ordered" || userTag === "ol") {
+//     return "ol";
+//   }
+//   else if (userTag === "unordered" || userTag === "ul") {
+//     return "ul";
+//   }
+//   else {
+//     alert("I don't understand");
+//   }
+// }
+
+// function listSkills(listType, skills) {
+//   const listElement = document.createElement(listType);
+//   document.body.appendChild(listElement);
   
-  for (const s of skills) {
-    listElement.innerHTML += `<li>${s}</li>`;
-  }
-}
+//   for (const s of skills) {
+//     listElement.innerHTML += `<li>${s}</li>`;
+//   }
+// }
+
+
+
+
+
+
+// Part Three
+// 1.
+const buttonElement = document.createElement("button");
+document.body.appendChild(buttonElement);
+
+buttonElement.addEventListener("click", () => {
+  document.body.style.backgroundColor = "cyan";
+})
